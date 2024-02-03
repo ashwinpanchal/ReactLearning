b0928d38c98930309cf3362f97bd5c9eefb4394d
ashwinpanchal <ashwinpanchal.iitb@gmail.com>
2024-02-03 16:13:19 +0530

    Array Rendering in React

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Learning React 
+
+## Day 1
+
+- `npm create vite@latest` creates a new react app for us
+- `npm run dev` used for hosting locally
+- `useState()` 
+- Components , thier name must start with a capital letter
+
+###### Rendereing an Array
+
+- We use `arr.map((value)=>{return component})`
--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,77 @@
-import { useState } from "react"
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+/* eslint-disable react/jsx-key */
+import { useState } from "react";
 
 function App() {
-  const [todo,setTodo] = useState({
-    title: "a",
-    id: 1,
-    description: "hello"
-  });
+  const [todo, setTodo] = useState([
+    {
+      title: "a",
+      id: 1,
+      description: "hello 1",
+    },
+    {
+      title: "ap",
+      id: 2,
+      description: "hello 2",
+    },
+    {
+      title: "appp",
+      id: 3,
+      description: "hello 3",
+    },
+    {
+      title: "apppppp",
+      id: 4,
+      description: "hello 4",
+    },
+  ]);
 
-  setTimeout(()=>{
-    let random = Math.random();
-    setTodo({title: "Joker",id: random,description: "sdasdasd"});
-  },2000);
+  setTimeout(() => {
+    setTodo([
+      {
+        title: "joekr 0",
+        id: 1,
+        description: "des4",
+      },
+      {
+        title: "joker 1",
+        id: 2,
+        description: "hfasdf",
+      },
+      {
+        title: "joker 2",
+        id: 3,
+        description: "helloasfasdfas 3",
+      },
+      {
+        title: "joker 3",
+        id: 4,
+        description: "hfasfsadello",
+      },
+    ]);
+  }, 5000);
 
   return (
     <>
-    <h1>Hello World!</h1>
-    <Todo todo={todo}/>
+      <h1>Hello World!</h1>
+      {todo.map((value) => {
+        return <Todo todo={value} />;
+      })}
     </>
-  )
+  );
 }
 
-function Todo(props){
-  return <div>
-    {props.todo.title}
-    <br/>
-    {props.todo.id}
-    <br/>
-    {props.todo.description}
-  </div>
+function Todo(props) {
+  return (
+    <div>
+      {props.todo.title} && {props.todo.id}
+      <br />
+      {props.todo.description}
+      <br />
+      <br />
+    </div>
+  );
 }
 
-export default App
+export default App;
